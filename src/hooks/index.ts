b95c283af1d77494c20f2a4c9332d31ce0d98d0f
Wout Vandesompele <33844638+wowtvds@users.noreply.github.com>
2023-06-07 23:13:11 +0200

    fix: rename file

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,6 @@
 export { default as useSpotify } from './useSpotify';
 export { default as useNotion } from './useNotion';
-export { default as useLocalStorage } from './useLocalStoragee';
+export { default as useLocalStorage } from './useLocalStorage';
 export { default as useAuth } from './useAuth';
 export { default as useEmail } from './useEmail';
 export { default as useOpenAi } from './useOpenAi';
