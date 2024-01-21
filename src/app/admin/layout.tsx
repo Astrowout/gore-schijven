05c8b584d4d78684651df0181bafcafee2fa5100
Wout Vandesompele <wowtvds@gmail.com>
2024-01-21 23:00:48 +0100

    fix: ts issue

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -18,7 +18,7 @@ export default async function AdminLayout ({
 }) {
     return (
         <main className='flex grow flex-col'>
-            <Hero title="GORE SCHIJVEN™">
+            <Hero>
                 <Button url="/">
                     Terug naar home
                 </Button>
